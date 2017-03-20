f28ba3e91c9b9b654521939584dc8667a3617cb9
Tom Cools <tomco@infosupport.com>
2017-03-20 18:22:23 +0100

    Added terminal

---
+++ vertxshell.js
@@ -0,0 +1,51 @@
+;(function() {
+
+  var VertxTerm = function(url, options, spawnElement) {
+    options = options || {};
+    var cols = options.cols || 80;
+    var rows = options.rows || 24;
+    url += (url.indexOf('?') > -1 ? '&' : '?') + "cols=" +  cols + "&rows=" + rows;
+    var termOptions = options.termOptions || { screenKeys: true };
+    termOptions.cols = cols;
+    termOptions.rows = rows;
+    var socket;
+    if (url.substring(0, 2) == 'ws') {
+      socket = new WebSocket(url);
+      socket.binaryType = 'blob';
+    } else {
+      socket = new SockJS(url);
+    }
+    socket.onopen = function () {
+      var term = new Terminal(termOptions);
+      socket.onmessage = function (event) {
+        if (event.type === 'message') {
+          console.log(typeof event.data);
+          if (typeof event.data !== 'string') {
+            var reader = new FileReader();
+            reader.onloadend = function() {
+              term.write(reader.result);
+            };
+            reader.readAsText(event.data);
+          } else {
+            term.write(event.data);
+          }
+        }
+      };
+      socket.onclose = function () {
+        socket.onmessage = null;
+        socket.onclose = null;
+        term.destroy();
+      };
+      term.on('data', function (data) {
+        socket.send(JSON.stringify({action: 'read', data: data}));
+      });
+      term.open(spawnElement);
+    };
+  };
+
+  if (typeof module !== 'undefined') {
+    module.exports = VertxTerm;
+  } else {
+    this.VertxTerm = VertxTerm;
+  }
+})();
